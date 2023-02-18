7ded123315b9babea3fa56dcaedd5d957ca91ae5
CodeCloneDL <qwer1314955811@163.com>
2023-02-18 22:06:43 +0800

    extract all the result of Diff command of all bug-fixing commit to file

--- src/main/java/features/ProcessCommit.java
+++ src/main/java/features/ProcessCommit.java
@@ -1,27 +1,31 @@
 package features;
 
-import org.apache.commons.math3.distribution.RealDistribution;
-
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class ProcessCommit {
     public static void main(String[] args) throws IOException {
         // 1. 提取commit 和bug-fixing commit,及对应的id
-        String path = "D:\\tmp"; // 每个项目的目录所在的目录; 自定义
-        ProcessCommit.calCommitNum(path);
+        String projectsDir = "D:\\tmp"; // 每个项目的目录所在的目录; 自定义
+        ProcessCommit.calCommitNum(projectsDir);
 
-        // 2.
+        // 2. 提取所有bug-fixing commit的diff结果到文件中;
+        String gitRepoPath = "D:\\GitRepository";
+        extractAllDiffOfBugFixingCommitToFile(projectsDir, gitRepoPath);
+
+        // 3. 从diff文件提取修改行;
     }
 
     // 功能： 1. 计算 'git log commit1..commit2'产生的commit信息文件中所有的commit数量
     // 2. 计算其中bug-fixing commit的数量
     // 3. 提取出bug-fixing 的所有commit id;
-    public static void calCommitNum(String path) throws IOException {
+    public static void calCommitNum(String projectsDir) throws IOException {
         // 用于bug-fixing commit的关键字匹配
         String[] keyWords = new String[]{"bug", "fix", "wrong", "error", "fail", "problem", "patch"};
 
-        File[] Dir = new File(path).listFiles(); // 项目目录所在目录;
+        File[] Dir = new File(projectsDir).listFiles(); // 项目目录所在目录;
         for (File project : Dir) { // 每次取一个项目目录
             String name = project.getName(); // 项目名
 
@@ -82,7 +86,6 @@ public class ProcessCommit {
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(str).find() && str.startsWith("commit");
     }
-
     // 检查关键字是否匹配;
     public  static boolean checkIfKeyWordsMatch(String str, String[] keyWords) {
         for (String keyWord : keyWords) {
@@ -90,4 +93,64 @@ public class ProcessCommit {
         }
         return false;
     }
+
+
+    // 功能2： 利用每个项目的所有bug-fixing Id，提取所有的bug_fixing commit的diff结果存到文件中;
+    public static void extractAllDiffOfBugFixingCommitToFile(String projectsDir, String gitRepoPath) throws IOException {
+        File[] projects = new File(projectsDir).listFiles();
+        for (File project : projects) { // 针对每一个项目，把所有bug-fixing commit的diff结果存到不同的文件;
+            String name = project.getName();
+            // 找到项目文件夹中的以_commitIds结尾的文件;
+            File target = null;
+            for (File file : project.listFiles()) {
+                if (file.getName().contains("_commitIds")) {
+                    target = file;
+                    break;
+                }
+            }
+
+            // 读取每个commitId，然后对这个commit使用 "git diff commitId commitId^"
+            // 求得这个修改行的信息;
+            BufferedReader bR = new BufferedReader(new FileReader(target));
+            String line;
+            while ((line = bR.readLine()) != null) {
+                // 拿到commit;
+                String commitId = line.split(" ")[0];
+                int index = Integer.parseInt(line.split(" ")[1]); // 第几个bug-fixing
+
+                // 开始执行命令行命令，将每个bug-fixing commmit的 diff内容求出，并得到一个bug_fixing_commit_index.txt文件;：
+                List<String> command = new ArrayList<>(); // 执行的命令;
+                command.add("pwsh");
+                command.add("-Command");
+                command.add("cd " + gitRepoPath + File.separator + name + "; git diff " + commitId + " " + commitId + "^");
+                implLongCommand(command, index, target); // 执行命令，将每个commitId对应的diff结果存到文件中;
+            }
+            bR.close();
+        }
+    }
+    // 执行一个长命令，把命令结果输出到target文件所在目录的新文件中，文件的名字格式为 "bug_fixing_commit_" + index;
+    public static void implLongCommand(List<String> command, int index, File target) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(target.getParent() + File.separator + "bug_fixing_commit_" + index + ".txt")));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line + "\n");
+            }
+
+            process.waitFor();
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
--- src/test/java/TestServeralFunc.java
+++ src/test/java/TestServeralFunc.java
@@ -1,25 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import org.apache.logging.log4j.message.StructuredDataId;
+
+import java.io.*;
+import java.util.LinkedList;
 
 public class TestServeralFunc {
-    public static void main(String[] args) {
-        String[] command = {"pwsh", "-Command", "cd ~; pwd; ls"};
-        ExeLongCommand(command);
+    static ProcessBuilder pB = new ProcessBuilder("pwsh", "-NoExit", "-NoLogo");
+    static Process process;
+    static OutputStream stdin;
+    static InputStream stdout;
+    public static void main(String[] args) throws IOException {
+        pB.directory(new File("D:\\tmp\\"));
+        pB.redirectErrorStream(true);
+
+        LinkedList<String> command = new LinkedList<>();
+        ExeLongCommand("cd D:\\tmp");
+        ExeLongCommand("pwd");
+        ExeLongCommand("cd D:\\Clash");
+        ExeLongCommand("pwd");
     }
     // 传入一个长命令，使用pwsh执行，并
-    public static void ExeLongCommand(String[] command) {
+    public static void ExeLongCommand(String command) {
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder(command);
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            process = pB.start();
+            stdin = process.getOutputStream();
+            stdout = process.getInputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+            writer.write(command);
+            writer.flush();;
+            writer.close();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
 
             process.waitFor();
+            reader.close();
 
         } catch (Exception e) {
             e.printStackTrace();
